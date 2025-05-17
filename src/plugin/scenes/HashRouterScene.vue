18f4e881122e162c0dcfcfb645de7aaf4704da22
Cristian Díaz <xtndiaz@gmail.com>
2025-05-16 20:23:57 -0500

    Adapt to new Navigational View structure in Hash Router Scene

--- src/plugin/scenes/HashRouterScene.vue
+++ src/plugin/scenes/HashRouterScene.vue
@@ -3,7 +3,6 @@ import { inject } from 'vue'
 import { type HashRouter } from '../models'
 import HashRouterModalScene from './HashRouterModalScene.vue'
 import type { NavigationBarVM } from '@vueties/view-models'
-import NavigationBar from '@vueties/bars/NavigationBar.vue'
 import NavigationalView from '@vueties/views/NavigationalView.vue'
 
 defineProps<{
@@ -18,28 +17,14 @@ function navigateToRoute(key: string) {
 </script>
 
 <template>
-  <NavigationalView class="base-view">
-    <template v-slot:bar>
-      <NavigationBar 
-        v-if="navigationBarVM.isVisible" 
-        :vm="navigationBarVM" 
-        @route-selected="navigateToRoute"
-      />
-    </template>
-    <template v-slot:view>
-      <component :is="hashRouter?.baseRoute?.value.view" />
-    </template>
+  <NavigationalView
+    :navigationBarVM="navigationBarVM"
+    @route-selected="navigateToRoute"
+  >
+    <component :is="hashRouter?.baseRoute?.value.view" />
   </NavigationalView>
   
   <HashRouterModalScene 
     :route="hashRouter?.modalRoute.value"
   />
 </template>
-
-<style scoped lang="scss">
-@use '@vueties/styles/utils';
-
-.base-view {
-  // @include utils.safe-area()
-}
-</style>
